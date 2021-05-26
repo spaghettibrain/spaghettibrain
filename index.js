2e849842d63e1f5b8a31e7095a8eb1c0c0f7cd10
Molly Beach <mollyjbeach@gmail.com>
2021-05-26 04:19:50 +0000

    added methods

--- index.js
+++ index.js
@@ -2,61 +2,49 @@
 /*************************************QUESTION********************************************/
 /***************************************************************************************/
 /*
-5. Longest Palindromic Substring
-Medium
-Given a string s, return the longest palindromic substring in s.
+1408. String Matching in an Array
+Easy
+Given an array of string words. Return all strings in words which is substring of another word in any order. 
+String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
 
 Example 1:
-Input: s = "babad"
-Output: "bab"
-Note: "aba" is also a valid answer.
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
 
 Example 2:
-Input: s = "cbbd"
-Output: "bb"
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
 
 Example 3:
-Input: s = "a"
-Output: "a"
-
-Example 4:
-Input: s = "ac"
-Output: "a"
+Input: words = ["blue","green","bu"]
+Output: []
+*/
 /*************************************************************************************/
 /************************************ANSWER********************************************/
 /*********************************************************************************/
-var longestPalindrome = function(s) {
-    let start = 0;
-    let end = 0;
-    let len = 0;
-   
-    //the helper function below finds the longest palindrome of the string
-    function isPalindromeLength(s, left, right){
-        if(left > right ) return 0;
-        while(left >=0 && right < s.length && s[left] == s[right]){
-            left --;
-            right++;
-        }
-        return right - left - 1
-    }
-    
-    
-     for(var i = 0 ; i  < s.length; i++){
-       let p1 = isPalindromeLength(s, i,i)
-       let p2 = isPalindromeLength(s, i, i+1)
-       len = Math.max(p1,p2)
-	   //the if statement below calculates the substring using the length of the longest palindrome 
-        if(len > end - start){
-          start  = Math.ceil(i - ((len-1)/2))/** here we subtract the (length-1)/2 from the current index and round it up. e.g when our length is 2, (2-1)/2=>0.5 then we round up using Math.ceil to 1 **/ 
-          end = i + (len/2)
-        } 
-        
-    }
-    
-    return s.slice(start, end + 1) 
+/*
+var stringMatching = function(words) {
+  let arr=[], res;
+  words = words.filter(elem => !elem);
+  for(e of words){
+  res = words.filter(elem => elem.includes(e));
+  res.sort((a, b) => b.length - a.length)
+  res.splice(0, 1);
+  arr = [...arr, ...res]
+}
+return arr
 };
-longestPalindrome(s = "cbbd");
+stringMatching(
+["leetcoder","leetcode","od","hamlet","am"]); 
+*/
 
 
 
+let chars = ['A', 'B', 'A', 'C', 'B'];
 
+let uniqueChars = chars.filter((c, index) => {
+    return chars.indexOf(c) === index;
+});
