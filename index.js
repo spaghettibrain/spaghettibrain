3c7e7c22f321ab70581456c2de9b41aa341baf6d
Molly Beach <mollyjbeach@gmail.com>
2021-05-27 22:20:32 +0000

    fix

--- index.js
+++ index.js
@@ -2,49 +2,72 @@
 /*************************************QUESTION********************************************/
 /***************************************************************************************
 /*
-1672. Richest Customer Wealth
+941. Valid Mountain Array
 Easy
-You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+Given an array of integers arr, return true if and only if it is a valid mountain array.
 
-A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+Recall that arr is a mountain array if and only if:
 
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
 Example 1:
-Input: accounts = [[1,2,3],[3,2,1]]
-Output: 6
-Explanation:
-1st customer has wealth = 1 + 2 + 3 = 6
-2nd customer has wealth = 3 + 2 + 1 = 6
-Both customers are considered the richest with a wealth of 6 each, so return 6.
+Input: arr = [2,1]
+Output: false
 
 Example 2:
-Input: accounts = [[1,5],[7,3],[3,5]]
-Output: 10
-Explanation: 
-1st customer has wealth = 6
-2nd customer has wealth = 10 
-3rd customer has wealth = 8
-The 2nd customer is the richest with a wealth of 10.
+Input: arr = [3,5,5]
+Output: false
 
 Example 3:
-Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
-Output: 17
-
+Input: arr = [0,3,2,1]
+Output: true
+/*
+Test Cases: 
+[2,0,2] -> 28. false
+[3,5,5] ->50. false 
+[0,1,2,4,2,1] --> 35. true
+[3,7,6,4,3,0,1,0] --> 33. false
+[0,1,2,3,4,5,6,7,8,9] --> 30. false
+[4,4,3,2,1] --> 52. false
+[9,8,7,6,5,4,3,2,1,0] —> 54. false
 */
+
 /*************************************************************************************/
 /************************************ANSWER********************************************/
 /*********************************************************************************/
-
-var reverseStr = function(s, k) {
-  for(const i in s){
-  if(i>k){k.pop();}
+//my answer works 
+/*
+var validMountainArray = function(arr) {
+  let ln = arr.length, peak=0;
+  if(!(arr.length >=3)) return false
+  for(let i=0; i<ln; i++){
+    let cur=arr[i], nex=arr[i+1];
+     if (cur===nex) return false
+     else if(cur> nex) peak+=1;
+     else if (peak>0 && ( i<1 || (!(cur>nex)&& i!=ln-1))) return false
+     else if (peak<1 && (!(cur<nex) || i===ln-1)) return false
   }
-  
-   return k
+return true   
 };
-reverseStr( s = "abcdefg", k = 2)
+validMountainArray([4,4,3,2,1]
 
-
-
-
-
+// a leetcode answer 
+var validMountainArray = function(arr) {
+    if(arr.length <3) return false;
+    var max = -Infinity;
+    var flag = true;
+    if(arr[0]>arr[1]) return false
+    for(let i=0; i<arr.length; i++){
+      let cur=arr[i], nex=arr[i+1];
+        if(flag && max < cur) max = cur
+        else flag = false; if(cur >= next)return false
+        
+      
+    }
+    if(flag) return false
+    return true
+};
+*/
