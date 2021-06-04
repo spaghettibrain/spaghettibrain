0caa22a303bc75874e4c5cb03ee8c8c221998a5a
Molly Beach <mollyjbeach@gmail.com>
2021-06-04 20:31:56 +0000

    temp

---
+++ algorithms/diameterTree.js
@@ -0,0 +1,94 @@
+/**************************************************************************************/
+/*************************************QUESTION********************************************/
+/***********************************************************************************/
+/*
+543. Diameter of Binary Tree
+Easy
+Given the root of a binary tree, return the length of the diameter of the tree.
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the tree.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+Example 1:
+Input: tree = [1,2,3,4,5]
+diameterOfBinaryTree( tree =
+{
+val: 1,
+    left: { val: 2, 
+                  left: { val: 4,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 5,        
+                                 left: null,
+                                 right: null
+                         }, 
+         },  
+  
+    right:  { val: 3,        
+                    left: null,
+                    right: null
+            }, 
+
+}
+) 
+Output: 3
+Explanation: 3is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+Input: tree = [1,2]
+diameterOfBinaryTree( tree =
+{
+val: 1,
+       left: { val: 2,        
+                      left: null,
+                      right: null
+              }, 
+  
+    right: null
+
+}
+) 
+Output: 1
+
+/*************************************************************************************/
+/************************************ANSWER********************************************/
+/***********************************************************************************/ 
+//call tree 
+//Time : O(n)
+//Space : Call stack O(n) linear 
+var diameterOfBinaryTree = function (tree) {
+  function search(tree) {
+    if (!tree) return [0, 0]; 
+    let [leftD, leftH] = search(tree.left);
+    let [rightD, rightH] = search(tree.right);
+    return [
+      Math.max(leftD, rightD, leftH + rightH),
+      Math.max(leftH, rightH) + 1,
+    ];
+  }
+  return search(tree)[0];
+};
+
+
+diameterOfBinaryTree( tree =
+{
+val: 1,
+    left: { val: 2, 
+                  left: { val: 4,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 5,        
+                                 left: null,
+                                 right: null
+                         }, 
+         },  
+  
+    right:  { val: 3,        
+                    left: null,
+                    right: null
+            }, 
+
+}
+) 
--- index.js
+++ index.js
@@ -6,94 +6,334 @@ let print = function (input){
 /*************************************QUESTION********************************************/
 /***********************************************************************************/
 /*
-543. Diameter of Binary Tree
+872. Leaf-Similar Trees
 Easy
-Given the root of a binary tree, return the length of the diameter of the tree.
-The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the tree.
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
 
-The length of a path between two nodes is represented by the number of edges between them.
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes tree1 and tree2 are leaf-similar.
+
 
 Example 1:
-Input: tree = [1,2,3,4,5]
-diameterOfBinaryTree( tree =
+Input: tree1 = [3,5,1,6,2,9,8,null,null,7,4], tree2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+leafSimilar( 
+tree1 =
 {
-val: 1,
-    left: { val: 2, 
-                  left: { val: 4,        
+val: 3,
+    left: { val: 5, 
+                  left: { val: 6,        
                                  left: null,
                                  right: null
                          }, 
-                  right: { val: 5,        
+                  right: { val: 2,        
+                                  left: { val: 7,        
+                                                left: null,
+                                                right: null
+                                          }, 
+                                  right: { val: 4,        
+                                                left: null,
+                                                right: null
+                                           }, 
+                          }, 
+         },  
+  
+ right: { val: 1, 
+                  left: { val: 9,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 8,        
+                                 left: null,
+                                 right: null
+                         }, 
+         }
+
+},
+
+tree2= 
+{
+val: 3,
+    left: { val: 5, 
+                  left: { val: 6,       
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 7,        
                                  left: null,
                                  right: null
                          }, 
          },  
   
-    right:  { val: 3,        
-                    left: null,
-                    right: null
-            }, 
+ right: { val: 1, 
+                  left: { val: 4,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 2,        
+                                  left: { val: 9,        
+                                                left: null,
+                                                right: null
+                                          }, 
+                                   right: { val: 8,        
+                                                 left: null,
+                                                 right: null
+                                           }, 
+                          }, 
+         }
 
 }
+
+
 ) 
-Output: 3
-Explanation: 3is the length of the path [4,2,1,3] or [5,2,1,3].
 
 Example 2:
-Input: tree = [1,2]
+Input: tree1 = [1], tree2 = [1]
+Output: true
 
-diameterOfBinaryTree( tree =
+Example 3:
+Input: tree1 = [1], tree2 = [2]
+Output: false
+leafSimilar( 
+tree1 =
+{ val: 1,        
+        left: null,
+        right: null
+}, 
+
+tree2 =
+{ val: 2,        
+        left: null,
+        right: null
+}
+
+) 
+
+Example 4:
+Input: tree1 = [1,2], tree2 = [2,2]
+Output: true
+leafSimilar( 
+tree1 =
 {
 val: 1,
-       left: { val: 2,        
-                      left: null,
-                      right: null
-              }, 
-  
+    left: { val: 2,        
+                  left: null,
+                  right: null
+           }, 
+
+    right: null
+
+},
+
+tree2= 
+{
+val: 2,
+    left: { val: 2,        
+                  left: null,
+                  right: null
+           }, 
+
     right: null
 
 }
+
 ) 
-Output: 1
 
-/*************************************************************************************/
-/************************************ANSWER********************************************/
-/***********************************************************************************/ 
-//call tree 
-//Time : O(n)
-//Space : Call stack O(n) linear 
-var diameterOfBinaryTree = function (tree) {
-  function search(tree) {
-    if (!tree) return [0, 0]; 
-    let [leftD, leftH] = search(tree.left);
-    let [rightD, rightH] = search(tree.right);
-    return [
-      Math.max(leftD, rightD, leftH + rightH),
-      Math.max(leftH, rightH) + 1,
-    ];
-  }
-  return search(tree)[0];
-};
+Example 5:
+Input: tree1 = [1,2,3], tree2 = [1,3,2]
+Output: false
 
-
-diameterOfBinaryTree( tree =
+Test Case : 
+19. [1], [2] --> False
+23. [1,2], [2,2] -> True
+24. [1,2,null,3], [1,3] -True
+25. [119,113,null,11,30,43,76,15,60,67,74] -> true
+[11,69,60,115,66,15,60,67,74,null,76]
+leafSimilar( 
+tree1 =
 {
 val: 1,
     left: { val: 2, 
-                  left: { val: 4,        
+                  left: { val: 3,        
+                                left: null,
+                                right: null
+                         },     
+                   right: null
+          },
+    right: null 
+
+},
+tree2 =
+{
+val: 1,
+    left: { val: 3, 
+                    left: null,    
+                    right: null     
+           }, 
+   right: null
+}
+)
+/*************************************************************************************/
+/************************************ANSWER********************************************/
+/***********************************************************************************/ 
+
+
+var leafSimilar = function(tree1, tree2) {
+const start = (tree, res) => {
+    if (!tree) {
+        return;
+    }
+    if (!tree.left && !tree.right) {
+        res.push(tree.val);
+        return;
+    }
+    start(tree.left, res);
+    start(tree.right, res);
+
+  return res
+}
+
+
+    let arr1= start(tree1, []);
+    let arr2= start(tree2, []);
+
+  print(arr1)
+
+      return  JSON.stringify(arr1) ===  
+              JSON.stringify(arr2);
+
+};
+
+leafSimilar( 
+tree1 =
+{
+val: 3,
+    left: { val: 5, 
+                  left: { val: 6,        
                                  left: null,
                                  right: null
                          }, 
-                  right: { val: 5,        
+                  right: { val: 2,        
+                                  left: { val: 7,        
+                                                left: null,
+                                                right: null
+                                          }, 
+                                  right: { val: 4,        
+                                                left: null,
+                                                right: null
+                                           }, 
+                          }, 
+         },  
+  
+ right: { val: 1, 
+                  left: { val: 9,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 8,        
+                                 left: null,
+                                 right: null
+                         }, 
+         }
+
+},
+
+tree2= 
+{
+val: 3,
+    left: { val: 5, 
+                  left: { val: 6,       
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 7,        
                                  left: null,
                                  right: null
                          }, 
          },  
   
-    right:  { val: 3,        
-                    left: null,
-                    right: null
-            }, 
+ right: { val: 1, 
+                  left: { val: 4,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 2,        
+                                  left: { val: 9,        
+                                                left: null,
+                                                right: null
+                                          }, 
+                                   right: { val: 8,        
+                                                 left: null,
+                                                 right: null
+                                           }, 
+                          }, 
+         }
 
 }
+)
+/*
+leafSimilar( 
+ tree1 =
+{
+val: 119,
+    left: { val: 113, 
+                  left: { val: 6,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: null, 
+         },  
+  
+ right: { val: 1, 
+                  left: { val: 9,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 8,        
+                                 left: null,
+                                 right: null
+                         }, 
+         }
+
+},
+
+tree2= 
+{
+val: 3,
+    left: { val: 5, 
+                  left: { val: 6,       
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 7,        
+                                 left: null,
+                                 right: null
+                         }, 
+         },  
+  
+ right: { val: 1, 
+                  left: { val: 4,        
+                                 left: null,
+                                 right: null
+                         }, 
+                  right: { val: 2,        
+                                  left: { val: 9,        
+                                                left: null,
+                                                right: null
+                                          }, 
+                                   right: { val: 8,        
+                                                 left: null,
+                                                 right: null
+                                           }, 
+                          }, 
+         }
+
+}
+
+
 ) 
+*/
+
